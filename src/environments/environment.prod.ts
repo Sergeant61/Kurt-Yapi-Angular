465ca361fd6f09c212f0b619d430ec9ff72455e7
Sergeant61 <rec1856rec@gmail.com>
2020-04-14 23:07:00 +0300

    p

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  path: 'https://kurt-yapi-api.herokuapp.com',
+  baseUrlOzelbinekarac: '/admin/ozelbinekarac',
+  baseUrlIsmakinesi: '/admin/ismakinesi',
+  baseUrlTirKamyon: '/admin/tirkamyon',
+  baseUrlFirma: '/admin/firma',
+  baseUrlPersonel: '/admin/personel',
+  baseUrlDokumSahasi: '/admin/dokumsahasi',
+  //--------
+  baseUrlTirKamyonCalisma: '/muhasebe/tirkamyon',
+  baseUrlIsMakinesiCalisma: '/muhasebe/ismakinesi',
+  baseUrlYakitFormu: '/muhasebe/yakitformu',
+  baseUrlGelenYakitFormu: '/muhasebe/gelenyakitformu',
+  baseUrlGidenYakitFormu: '/muhasebe/gidenyakitformu'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   path: 'https://kurt-yapi-api.herokuapp.com',
   baseUrlOzelbinekarac: '/admin/ozelbinekarac',
   baseUrlIsmakinesi: '/admin/ismakinesi',
