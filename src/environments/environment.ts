e18a9bda41da119186e10920969080c23d22270f
Sergeant61 <rec1856rec@gmail.com>
2020-04-24 23:12:13 +0300

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  path1: 'https://kurt-yapi-api.herokuapp.com',
-  path: 'http://192.168.8.150:3001',
+  path: 'https://kurt-yapi-api.herokuapp.com',
+  path1: 'http://192.168.8.150:3001',
   baseUrlOzelbinekarac: '/admin/ozelbinekarac',
   baseUrlIsmakinesi: '/admin/ismakinesi',
   baseUrlTirKamyon: '/admin/tirkamyon',
